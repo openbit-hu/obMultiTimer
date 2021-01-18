9b2a8839c323e673d884e3f9924f6f7918332348
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-01-18 21:26:24 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,63 @@
+class Timer{
+    id:string
+    dt:number
+    t0:number
+    timerCallback:()=>void
+    constructor(id:string,dt:number,body:()=>void){
+        this.id=id
+        this.dt=dt
+        this.timerCallback=body
+    }
+    start(millis:number){
+        this.t0=millis
+    }
+    check(t:number){
+        if(t-this.t0>this.dt){
+            this.timerCallback()
+            this.t0=t
+        }
+    }
+}
+
+//% color=#008060 weight=100 icon="\uf017" block="obTimer"
+namespace obMultiTimer {
+    let timers:Timer[]=[]
+    let minDt:number=1000
+    let res:number=200
+    let t0:number
+    //% blockId="obTimer_start"
+    //% block="start Timer event || in every $time ms with $resolution ms accuracy"
+    export function start(resolution?:number){
+        if(resolution)res=resolution
+//        if(resolution>dt/2)resolution=dt/2
+        t0=control.millis()
+        for(let timer of timers){
+            timer.start(t0)
+        }
+        control.inBackground(function () {
+            while(true){
+                basic.pause(res)
+                let t1=control.millis()
+                for(let timer of timers){
+                    timer.check(t1)
+                }
+            }
+        })
+    }
+    /**
+     * Attaches code to run when the timer triggers an event.
+     * @param id name of the timer
+     * @param dt time difference between calls
+     * @param body TODO
+     */
+    //% blockId="obTimer_onTimerEvent"
+    //% block="onTimerEvent"
+    export function onTimerEvent(id:string, dt:number, body: () => void): void {
+        if(dt<100)dt=100
+        if(dt<minDt){
+            minDt=dt
+            res=minDt/2
+        }
+        timers.push(new Timer(id,dt,body))
+    }
+}
